f51adefd4cae277e4ec864a0510f6623ea598f43
Ammar Al-Khawaldeh <me@ammar.dev>
2023-08-25 23:28:49 +0300

    Initial Commit

---
+++ src/JsonHttpClient.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AmmarDev\HttpClient;
+
+class JsonHttpClient
+{
+    public static function get(string $url): array
+    {
+        $curlHandle = curl_init();
+
+        curl_setopt_array($curlHandle, [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+        ]);
+
+        $response = curl_exec($curlHandle);
+
+        curl_close($curlHandle);
+
+        return json_decode($response, true);
+    }
+}
